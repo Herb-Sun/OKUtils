5ac975f1fba59f82e8ea4707798467bfc985f1ba
herb <herbsun@163.com>
2017-10-19 00:14:32 +0800

    update

--- OKUtils/OKUtils/OKMacro.h
+++ OKUtils/OKUtils/OKMacro.h
@@ -9,6 +9,8 @@
 #ifndef OKMacro_h
 #define OKMacro_h
 
+#pragma mark - LOG
+
 #ifndef OKLOG_ENABLED
 #define OKLOG_ENABLED DEBUG
 #endif
@@ -17,15 +19,18 @@
     #define __OKLOG(s, ...) NSLog(@"%@",[NSString stringWithFormat:(s), ##__VA_ARGS__])
     #define OKLogV() __OKLOG(@"\n✅ {%@} %@ > %s Line:(%d)\n", [NSThread isMainThread] ? @"UI" : @"BG", \
                                 [NSURL URLWithString:@__FILE__].lastPathComponent, __FUNCTION__, __LINE__)
-    #define OKLog(id, ...) __OKLOG(@"\n✅ {%@} %@ > %s Line:(%d): %@ \n", \
+    #define OKLog(s, ...) __OKLOG(@"\n✅ {%@} %@ > %s Line:(%d): %@ \n", \
                                     [NSThread isMainThread] ? @"UI" : @"BG", \
                                     [NSURL URLWithString:@__FILE__].lastPathComponent, \
                                     __FUNCTION__, __LINE__, \
-                                    [NSString stringWithFormat:(id), ##__VA_ARGS__])
+                                    [NSString stringWithFormat:(s), ##__VA_ARGS__])
 #else
+    #define __OKLOG(s, ...) do{} while(0)
     #define OKLogV() do{} while(0)
     #define OKLog(...) do{} while(0)
 #endif
+
+#pragma mark - WEAK SELF
 
 #ifndef OKWeakify
     #if __has_feature(objc_arc)
@@ -43,27 +48,36 @@
     #endif
 #endif
 
+#ifndef OKBLOCK_SAFE_EXEC
+#define OKBLOCK_SAFE_EXEC(block, ...) if(block){block(__VA_ARGS__);}
+#endif
 
-#define OKTABBAR_H        49.0f
-#define OKSTATUSBAR_H     20.0f
-#define OKNAVBAR_H        64.5f
+#ifndef OKFormatString
+#define OKFormatString(str,...) [NSString stringWithFormat:str, ##__VA_ARGS__]
+#endif
+
+#ifndef OKExecOnce
+#define OKExecOnce(block) \
+{ \
+static dispatch_once_t predicate = 0; \
+dispatch_once(&predicate, block); \
+}
+#endif
 
 #define OKTICK NSDate *startTime = [NSDate date];
 #define OKTOCK NSLog(@"Time:%f", -[startTime timeIntervalSinceNow]);
 
-/** 获取屏幕尺寸、宽度、高度 */
+#pragma mark - SIZE
+
 #define OKSCREEN_RECT   ([[UIScreen mainScreen] bounds])
 #define OKSCREEN_SIZE   ([UIScreen mainScreen].bounds.size)
 #define OKSCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
 #define OKSCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 
-/// 安全执行block
-#define OKBLOCK_SAFE_EXEC(block, ...) if(block){block(__VA_ARGS__);}
-#define OKFormatString(str,...) [NSString stringWithFormat:str, ##__VA_ARGS__]
+#pragma mark - SINGLETON
 
 #define OKSINGLETON_H + (instancetype)sharedInstance;
 #define OKSINGLETON_H_(methodName) + (instancetype)methodName;
-
 #define OKSINGLETON_M \
 static id _instance = nil; \
 + (instancetype)sharedInstance { \
